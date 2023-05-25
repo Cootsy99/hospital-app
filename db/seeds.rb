843f550c6ce5690cf81cd40ac145966a767ee056
Jack Coots <jack.coots@newtoneurope.com>
2023-05-25 20:40:55 +0100

    Require 'Faker' Seed Database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Patient.create(first_name: ,last_name: , diagnosis: , born_on:)
+
+require 'faker'
+
+illnesses = [
+    "Common cold",
+    "Influenza",
+    "Pneumonia",
+    "Bronchitis",
+    "Strep throat",
+    "Sinusitis",
+    "Urinary tract infection",
+    "Conjunctivitis",
+    "Migraine",
+    "Allergies",
+    "Asthma",
+    "Diabetes",
+    "Hypertension",
+    "Arthritis",
+    "Osteoporosis"
+  ]
+
+
+10.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  diagnosis = illnesses.sample
+  born_on = Faker::Date.birthday(min_age: 18, max_age: 65)
+
+  Patient.create(
+    first_name: first_name,
+    last_name: last_name,
+    diagnosis: diagnosis,
+    born_on: born_on
+  )
+end
